d2d4a0fadda08f839c4d6a611c97754311ec4cd3
luckyash33 <96337764+luckyash33@users.noreply.github.com>
2026-09-21 15:13:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "existing_instance" {
   ami           = "ami-0db56f446d44f2f09"
-  instance_type = "c7i-flex.large"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "tf-managed-instance"
